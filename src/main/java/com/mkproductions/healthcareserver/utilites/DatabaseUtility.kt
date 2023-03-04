a092f33a0e24530039931a4c092b4e7bc5ac519e
Mardin Kamble <kamblemardin@gmail.com>
2023-03-04 08:36:17 +0530

    Deleted everthing

--- src/main/java/com/mkproductions/healthcareserver/utilites/DatabaseUtility.kt
+++
@@ -1,9 +0,0 @@
-package com.mkproductions.healthcareserver.utilites
-
-<<<<<<< HEAD
-class DatabaseUtility{
-    
-}
-=======
-class DatabaseUtility
->>>>>>> 7e5ecd1cd6b18d422cbf35c02c6aee3c9e4efb25
